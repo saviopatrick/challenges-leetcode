39fd52848e853d70e8f695223e8aa8fb04dbd259
saviopatrick <saviopatrick09@gmail.com>
2024-05-11 21:39:20 -0300

    feat: add  find index question

---
+++ 028Find-Index.py
@@ -0,0 +1,12 @@
+#Given two strings needle and haystack, return the index of the first 
+#occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i + len(needle)] == needle:
+                return i
+        return -1
